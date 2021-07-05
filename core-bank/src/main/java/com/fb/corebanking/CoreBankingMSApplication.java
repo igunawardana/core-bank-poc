1c35a6d8c1719c749922a89aa383bb69aa8b46c6
Isuru Gunawardana <isuru.gnw@gmail.com>
2021-07-05 14:57:10 +0530

    working all 3 apis

--- core-bank/src/main/java/com/fb/corebanking/CoreBankingMSApplication.java
+++ core-bank/src/main/java/com/fb/corebanking/CoreBankingMSApplication.java
@@ -1,13 +1,21 @@
 package com.fb.corebanking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
+@EnableWebMvc
+@Configuration
+@ComponentScan
 public class CoreBankingMSApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoreBankingMSApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CoreBankingMSApplication.class, args);
+  }
 
 }
